70fb4803b9e2e044e3b021fa9ee53f5d5cad19a3
ryokotmng <ryokotmng@gmail.com>
2021-09-22 23:02:42 +0900

    Accept multiple statements separated by semicolons

--- parse.c
+++ parse.c
@@ -4,6 +4,8 @@
 
 #include "chibicc.h"
 
+static Node *expr(Token **rest, Token *tok);
+static Node *expr_stmt(Token **rest, Token *tok);
 static Node *equality(Token **rest, Token *tok);
 static Node *relational(Token **rest, Token *tok);
 static Node *add(Token **rest, Token *tok);
@@ -36,8 +38,20 @@ static Node *new_num(int val) {
     return node;
 }
 
+// stmt = expr-stmt
+static Node *stmt(Token **rest, Token *tok) {
+    return expr_stmt(rest, tok);
+}
+
+// expr-stmt = expr ";"
+static Node *expr_stmt(Token **rest, Token *tok) {
+    Node *node = new_unary(ND_EXPR_STMT, expr(&tok, tok));
+    *rest = skip(tok, ";");
+    return node;
+}
+
 // expr = equality
-Node *expr(Token **rest, Token *tok) {
+static Node *expr(Token **rest, Token *tok) {
     return equality(rest, tok);
 }
 
@@ -160,9 +174,11 @@ static Node *primary(Token **rest, Token *tok) {
     error_tok(tok, "expected an expression");
 }
 
+// program = stmt*
 Node *parse(Token *tok) {
-    Node *node = expr(&tok, tok);
-    if (tok->kind != TK_EOF)
-        error_tok(tok, "extra token");
-    return node;
+    Node head = {};
+    Node *cur = &head;
+    while (tok->kind != TK_EOF)
+        cur = cur->next = stmt(&tok, tok);
+    return head.next;
 }
