a0675a35e945de5861954c396cf3991b740ab9a1
ryokotmng <ryokotmng@gmail.com>
2021-09-22 18:47:14 +0900

    Compile an integer to an exectuable that exits with the given number

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+// Created by tominaga.ryoko on 2021/09/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+        return 1;
+    }
+
+    printf("  .globl main\n");
+    printf("main:\n");
+    printf("  mov $%d, %%rax\n", atoi(argv[1]));
+    printf("  ret\n");
+    return 0;
+}
